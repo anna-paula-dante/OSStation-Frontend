8c8b41318b10268204178a7f156208a1c0bd460f
Ana Paula <anajornalistasbc@gmail.com>
2025-08-20 21:34:04 -0300

    Feat: create a base project

---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+  return (
+    <h1 className="font-Inter text-white font-medium text-2xl">
+      <span className="text-[#00FFA3]">OS</span>Station
+    </h1>
+  );
+}
